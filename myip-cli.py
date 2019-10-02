256fef425c320577d664b0d0f2ad407e962d3931
Daniel Bohry <daniel.bohry@infinitec.solutions>
2019-10-02 14:37:18 +0200

    send data to server

--- myip-cli.py
+++ myip-cli.py
@@ -1,9 +1,20 @@
 import socket
 import threading
-def get_ip_address():
-    threading.Timer(5.0, get_ip_address).start()
+import requests
+
+
+def send_address():
+    threading.Timer(5.0, send_address).start()
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    print(s.getsockname()[0])
+    send_data("chip1", s.getsockname()[0])
 
-get_ip_address()
+
+def send_data(name, ip):
+    url = 'https://chip-ip.herokuapp.com/api/ip'
+    payload = '{"ip":"' + ip + '","name":"' + name + '"}'
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.post(url, data=payload, headers=headers)
+
+
+send_address()
